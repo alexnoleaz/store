a3fd502ebdf665e379e96d9a2fcdb58506c25b60
Alexander Nole <cnole_azahache@outlook.com>
2024-12-13 16:27:06 -0500

    feat(shared): implement automapper object mapper

---
+++ aspnet-core/src/Store.Domain/Shared/IObjectMapper.cs
@@ -0,0 +1,33 @@
+namespace Store.Shared;
+
+/// <summary>
+/// Defines methods for mapping objects between different types and projecting queries to a target type.
+/// </summary>
+public interface IObjectMapper
+{
+    /// <summary>
+    /// Maps an object to a new instance of the specified destination type.
+    /// </summary>
+    /// <typeparam name="TDestination">The type of the destination object.</typeparam>
+    /// <param name="source">The source object to map.</param>
+    /// <returns>A new instance of <typeparamref name="TDestination"/>.</returns>
+    TDestination Map<TDestination>(object source);
+
+    /// <summary>
+    /// Maps the source object to an existing instance of the specified destination type.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source object.</typeparam>
+    /// <typeparam name="TDestination">The type of the destination object.</typeparam>
+    /// <param name="source">The source object to map.</param>
+    /// <param name="destination">The existing destination object to populate.</param>
+    /// <returns>The populated <typeparamref name="TDestination"/> instance.</returns>
+    TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+
+    /// <summary>
+    /// Projects a queryable source to the specified destination type.
+    /// </summary>
+    /// <typeparam name="TDestination">The type of the destination objects in the query.</typeparam>
+    /// <param name="source">The queryable source to project.</param>
+    /// <returns>A queryable collection of <typeparamref name="TDestination"/>.</returns>
+    IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source);
+}
---
+++ aspnet-core/src/Store.Infrastructure/Shared/AutoMapperObjectMapper.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using Store.Shared.Dependency;
+
+namespace Store.Shared;
+
+/// <summary>
+/// Provides an implementation of <see cref="IObjectMapper"/> using AutoMapper.
+/// </summary>
+public class AutoMapperObjectMapper : IObjectMapper, ISingletonDependency
+{
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AutoMapperObjectMapper"/> class.
+    /// </summary>
+    /// <param name="mapper">The AutoMapper <see cref="IMapper"/> instance to use.</param>
+    public AutoMapperObjectMapper(IMapper mapper) => _mapper = mapper;
+
+    /// <inheritdoc />
+    public TDestination Map<TDestination>(object source) => _mapper.Map<TDestination>(source);
+
+    /// <inheritdoc />
+    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination) =>
+        _mapper.Map(source, destination);
+
+    /// <inheritdoc />
+    public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source) =>
+        _mapper.ProjectTo<TDestination>(source);
+}
