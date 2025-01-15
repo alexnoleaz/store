7ffa3a0a6488670258188fcab0183a111ad08fe2
Alexander Nole <cnole_azabache@outlook.com>
2025-01-14 19:30:16 -0500

    feat(efcore): add initial migration for Entity Framework Core

---
+++ aspnet-core/src/Store.Infrastructure/Shared/Repositories/EntityFrameworkCore/Migrations/20250115001348_initial-migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Infrastructure.Shared.Repositories.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
